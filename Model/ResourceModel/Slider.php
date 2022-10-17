eca49a1423e731de433bb0598403665c1e2cd83f
jahazaib321 <jahazaib@mage4.com>
2022-10-17 17:27:40 +0500

    first commit

---
+++ Model/ResourceModel/Slider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Mage4\ImageComparisonSlider\Model\ResourceModel;
+
+/**
+ * Class Store
+ */
+class Slider extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+    /**
+     * Init
+     */
+    protected function _construct() // phpcs:ignore PSR2.Methods.MethodDeclaration
+    {
+        $this->_init('mage4_comparisonslider_managesliders', 'id');
+    }
+}
+
---
+++ Model/Slider.php
@@ -0,0 +1,29 @@
+<?php
+namespace Mage4\ImageComparisonSlider\Model;
+
+/**
+ * Class Slider
+ */
+class Slider extends \Magento\Framework\Model\AbstractModel implements
+    \Mage4\StoreLocator\Api\Data\StoreInterface,
+    \Magento\Framework\DataObject\IdentityInterface
+{
+    const CACHE_TAG = 'image_comparison_slider';
+
+    /**
+     * Init
+     */
+    protected function _construct() // phpcs:ignore PSR2.Methods.MethodDeclaration
+    {
+        $this->_init(\Mage4\ImageComparisonSlider\Model\ResourceModel\Slider::class);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIdentities()
+    {
+        return [self::CACHE_TAG . '_' . $this->getId()];
+    }
+}
+
