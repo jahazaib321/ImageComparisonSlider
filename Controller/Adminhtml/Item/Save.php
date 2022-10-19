d66a98caf292a986ad75a448d300815246b8580f
jahazaib321 <jahazaib@mage4.com>
2022-10-19 22:56:41 +0500

    save controller

--- Controller/Adminhtml/Item/Save.php
+++ Controller/Adminhtml/Item/Save.php
@@ -3,42 +3,28 @@
 namespace Mage4\ImageComparisonSlider\Controller\Adminhtml\Item;
 
 use Mage4\ImageComparisonSlider\Model\SliderFactory;
-use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
-use Magento\Framework\Api\DataObjectHelper;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Model\AbstractModel;
 
-class Save extends Action
+class Save extends \Magento\Backend\App\Action
 {
     private $sliderFactory;
 
     public function __construct(
-        Context $context,
-        SliderFactory                       $sliderFactory,
-        DataObjectHelper                    $dataObjectHelper
-    )
-    {
+        \Magento\Backend\App\Action\Context $context,
+        SliderFactory $sliderFactory
+    ) {
         $this->sliderFactory = $sliderFactory;
-        $this->dataObjectHelper = $dataObjectHelper;
         parent::__construct($context);
     }
 
     public function execute()
     {
-        $data = $this->getRequest()->getPost('general');
+        $data = $this->getRequest()->getPostValue();
+        unset($data['id']);
+        $this->sliderFactory->create()
+            ->setData($data)
+            ->save();
 //        dd($data);
-        $model = $this->sliderFactory->create();
-        $id = $this->getRequest()->getParam('id');
-        $model->load($id);
-//        $data = $this->sliderFactory->create()
-//            ->setData($this->getRequest()->getPostValue('general'))
-//            ->save();
-//        dd($data);
-//        return $this->resultRedirectFactory->create()->setPath('comparisonslider/index/index');
-         $model->setData($data)->save();
-         return $this->resultRedirectFactory->create()->setPath('comparisonslider/index/index');
+        return $this->resultRedirectFactory->create()->setPath('comparisonslider/index/index');
     }
 }
 ?>
-
