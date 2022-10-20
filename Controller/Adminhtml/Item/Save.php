2d56c29771c384fd5edf221b77b18724e9db7f3b
jahazaib321 <jahazaib@mage4.com>
2022-10-20 19:37:20 +0500

    image work

--- Controller/Adminhtml/Item/Save.php
+++ Controller/Adminhtml/Item/Save.php
@@ -2,29 +2,57 @@
 
 namespace Mage4\ImageComparisonSlider\Controller\Adminhtml\Item;
 
+use Mage4\ImageComparisonSlider\Model\ImageUploader;
 use Mage4\ImageComparisonSlider\Model\SliderFactory;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Model\AbstractModel;
 
-class Save extends \Magento\Backend\App\Action
+class Save extends Action
 {
     private $sliderFactory;
+    private $imageUploader;
 
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        SliderFactory $sliderFactory
-    ) {
+        Context          $context,
+        SliderFactory     $sliderFactory,
+        ImageUploader    $imageUploader,
+        DataObjectHelper $dataObjectHelper
+    )
+    {
         $this->sliderFactory = $sliderFactory;
+        $this->imageUploader = $imageUploader;
+        $this->dataObjectHelper = $dataObjectHelper;
         parent::__construct($context);
     }
 
+
+    /**
+     * @throws LocalizedException
+     */
     public function execute()
     {
         $data = $this->getRequest()->getPostValue();
         unset($data['id']);
-        $this->sliderFactory->create()
-            ->setData($data)
-            ->save();
-//        dd($data);
+        $model = $this->sliderFactory->create();
+        $id = $this->getRequest()->getParam('id');
+        $model->load($id);
+        if ($data) {
+            if (isset($data['image'][0]['name']) && isset($data['image'][0]['tmp_name'])) {
+                $data['image'] = $data['image'][0]['name'];
+                /** @var ImageUploader $this- >imageUploader */
+                $this->imageUploader->moveFileFromTmp($data['image']);
+                $this->dataObjectHelper->populateWithArray($model, $data, AbstractModel::class);
+            } elseif (isset($data['image'][0]['name']) && !isset($data['image'][0]['tmp_name'])) {
+                $data['image'] = $data['image'][0]['name'];
+            } else {
+                $data['image'] = null;
+            }
+            $model->setData($data)->save();
+        }
         return $this->resultRedirectFactory->create()->setPath('comparisonslider/index/index');
     }
 }
-?>
+
