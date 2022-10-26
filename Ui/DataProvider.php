6d445edcb26002f87fc849d6a1b2c00ca9133b03
jahazaib321 <jahazaib@mage4.com>
2022-10-26 17:44:30 +0500

    image data provider

--- Ui/DataProvider.php
+++ Ui/DataProvider.php
@@ -24,17 +24,53 @@ class DataProvider extends AbstractDataProvider
         $result = [];
         foreach ($this->collection->getItems() as $item) {
             $data = $item->getData();
-//            if (isset($data['image'])) {
-//                $name = $data['image'];
-//                unset($data['image']);
-//                $data['image'][0] = [
-//                    'name' => $name,
-//                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $name
-//                ];
-//            }
+            if (isset($data['before_desktop_image'])) {
+                $before_desktop_image = $data['before_desktop_image'];
+                unset($data['before_desktop_image']);
+                $data['before_desktop_image'][0] = [
+                    'name' => $before_desktop_image,
+                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $before_desktop_image
+                ];
+            }
+
+            if (isset($data['after_desktop_image'])) {
+                $after_desktop_image = $data['after_desktop_image'];
+                unset($data['after_desktop_image']);
+                $data['after_desktop_image'][0] = [
+                    'name' => $after_desktop_image,
+                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $after_desktop_image
+                ];
+            }
+
+            if (isset($data['before_mobile_image'])) {
+                $before_mobile_image = $data['before_mobile_image'];
+                unset($data['before_mobile_image']);
+                $data['before_mobile_image'][0] = [
+                    'name' => $before_mobile_image,
+                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $before_mobile_image
+                ];
+            }
+
+            if (isset($data['after_mobile_image'])) {
+                $after_mobile_image = $data['after_mobile_image'];
+                unset($data['after_mobile_image']);
+                $data['after_mobile_image'][0] = [
+                    'name' => $after_mobile_image,
+                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $after_mobile_image
+                ];
+            }
+
+            if (isset($data['drag_icon'])) {
+                $dragicon = $data['drag_icon'];
+                unset($data['drag_icon']);
+                $data['drag_icon'][0] = [
+                    'name' => $dragicon,
+                    'url' => 'http://jahazaib.local/pub/media/' . 'mage4/comparisonslider/image/' . $dragicon
+                ];
+            }
             $result[$item->getId()] = $data;
         }
-//        dd($result);
+//        dd($data);
         return $result;
     }
 }
